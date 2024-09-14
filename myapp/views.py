89fd1a8b43b85725dcabc5cca91e63f7004317fa
Harsh Sinha <58680300+harsh6975@users.noreply.github.com>
2024-09-14 17:20:07 +0530

    passing variables to template

--- myapp/views.py
+++ myapp/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 def index(request):
     # return HttpResponse("THis is my app")
-    return render(request, 'home.html')
+    context ={
+        "pageName": "Home Page",
+    }
+    return render(request, 'home.html', context)
 
 def about(reques):
     return HttpResponse("THis is my about page")
