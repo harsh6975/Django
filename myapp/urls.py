c5e9dfcbd60b391b772d93c1c3f3bddd0090a23a
Harsh Sinha <58680300+harsh6975@users.noreply.github.com>
2024-09-14 16:54:22 +0530

    url dispatching

--- myapp/urls.py
+++ myapp/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from myapp import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('myapp.urls'))
+   path('',views.index,name='myapp'),
+   path('about',views.index,name='about')
 ]
---
+++ myapp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render,HttpResponse
+
+# Create your views here.
+def index(reques):
+    return HttpResponse("THis is my app")
+
+def index(reques):
+    return HttpResponse("THis is my about page")
