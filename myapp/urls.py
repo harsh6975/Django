faa73a3ab10e5a925407c1be96c62e5fd51ca022
Harsh Sinha <58680300+harsh6975@users.noreply.github.com>
2024-09-14 17:48:41 +0530

    contact page

--- myapp/urls.py
+++ myapp/urls.py
@@ -19,6 +19,8 @@ from django.urls import path
 from myapp import views
 
 urlpatterns = [
-   path('',views.index,name='myapp'),
-   path('about',views.about,name='about')
+    path('',views.index,name='myapp'),
+    path('about',views.about,name='about'),
+    path('contact',views.contact,name='contact')
+
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -10,3 +10,6 @@ def index(request):
 
 def about(reques):
     return HttpResponse("THis is my about page")
+
+def contact(request):
+    return render(request, 'contact.html')
