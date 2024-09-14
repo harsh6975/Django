344aaa14819ffe723edb148dbbe8308ba4e3a558
Harsh Sinha <58680300+harsh6975@users.noreply.github.com>
2024-09-14 18:08:20 +0530

    view change in db

--- myapp/models.py
+++ myapp/models.py
@@ -6,3 +6,6 @@ class Contact(models.Model):
     email = models.CharField(max_length=122)
     phone = models.CharField(max_length=13)
     desc = models.TextField()
+
+    def __str__(self):
+        return self.name
